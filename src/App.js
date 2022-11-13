f80792f19ddcbdeeca9075e05f84931406d0f943
immortallight <raythymson2018@gmail.com>
2022-11-13 12:08:39 +0100

    todoAapp completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css"
+// import cancelBt from "./cancel.png";
+// import todoIcon from "./src/to-do.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+ 
+function App(){
+
+// state hook _ "useState"
+const [newItem, setNewItem] = useState("");
+const [items, setItems]= useState([]);
+
+// helper function
+function addItem(){
+  if (!newItem) {
+    alert("Press enter an item.");
+    return;
+  }
+
+ const item = {
+  id: Math.floor(Math.random() * 1000),
+  value: newItem,
+ };
+
+
+
+ setItems((oldList) => [...oldList, item])
+ setNewItem("");
 }
+function removeItem (id) {
+  const newArray = items.filter((item) => item.id !== id);
+  setItems(newArray);
+}
+return(
+  <div className="App">
+{/* Header  */}
+<h1 className="header">
+<img src={require( "./to-do.png")} height="40" width="40" alt="fdvfgf"/>
+  
+  Todo List App</h1>
+{/* input  (input and button) */}
+<input 
+type="text"
+placeholder="Add an item...."
+value={newItem}
+onChange={(e) => setNewItem(e.target.value)}
+
+/>
+
+<button onClick={() => addItem()}>Add</button>
+
+{/* List of Items */}
+
+<ul >
+  {items.map(item => {
+    return(
+      <li key={item.id} >
+        {item.value}
+        <img src={require("./cancel.png")}
+         height="18" width="18" alt="fgefb"
+         className="delete-button" onClick={() => removeItem(item.id)}
+         />
+       
+      </li>
+    )
+  })}
+
+</ul>
+
+  </div>
+)
+
+}
+
+
+
+
 
 export default App;
