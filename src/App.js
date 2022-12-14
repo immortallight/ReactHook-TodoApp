b09199a0261f26e551914cb6b2842b140b00fa57
kaf-lamed-beyt <belac335@gmail.com>
2022-12-15 00:02:19 +0100

    feat: add input toggle feature

--- src/App.js
+++ src/App.js
@@ -1,91 +1,84 @@
-import React, {useState} from "react";
-import "./App.css"
+import React, { useState } from "react";
+import "./App.css";
+import { MdOutlineCancel } from "react-icons/md";
+import { AiOutlinePlus } from "react-icons/ai";
 
+function App() {
+  // state hook _ "useState"
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([]);
+  const [display, setDisplay] = useState(false);
 
+  // helper function
+  function addItem() {
+    if (!newItem) {
+      alert("Press enter an item.");
+      return;
+    }
 
- 
-function App(){
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem,
+    };
 
-// state hook _ "useState"
-const [newItem, setNewItem] = useState("");
-const [items, setItems]= useState([]);
-
-// helper function
-function addItem(){
-  if (!newItem) {
-    alert("Press enter an item.");
-    return;
+    setItems((oldList) => [...oldList, item]);
+    setNewItem("");
   }
 
- const item = {
-  id: Math.floor(Math.random() * 1000),
-  value: newItem,
- };
+  function removeItem(id) {
+    const newArray = items.filter((item) => item.id !== id);
+    setItems(newArray);
+  }
 
+  function handleInputDisplay() {
+    setDisplay(true);
+  }
 
+  return (
+    <div className="App">
+      {/* Header  */}
+      <h1 className="header">My Todo</h1>
 
- setItems((oldList) => [...oldList, item])
- setNewItem("");
-}
-function removeItem (id) {
-  const newArray = items.filter((item) => item.id !== id);
-  setItems(newArray);
+      {/* input  (input and button) */}
 
+      <div className="addNew">
+        <p className="intro">
+          My Todo helps you stay organized and perform your tasks more faster
+        </p>
 
-}
-return(
-  <div className="App">
-{/* Header  */}
-<h1 className="header">
-  
-  My Todo</h1>
+        {display ? (
+          <div className="input-and-button">
+            <input
+              type="text"
+              placeholder="Add an item...."
+              value={newItem}
+              onChange={(e) => setNewItem(e.target.value)}
+            />
+            <button onClick={() => addItem()}>Add</button>
+          </div>
+        ) : null}
 
-{/* input  (input and button) */}
+        {/* List of Items */}
 
-<div className="addNew" >
-<p className="intro">My Todo helps you stay organized and perform your tasks more faster</p>
-
-  <div className="input-and-button">
-  <input 
-      type="text"
-      placeholder="Add an item...."
-      value={newItem}
-      onChange={(e) => setNewItem(e.target.value)
-      }
-
-      />
-      <button onClick={() => addItem()}>Add</button>
+        <ul>
+          {items.map((item) => {
+            return (
+              <li key={item.id}>
+                {item.value}
+                <MdOutlineCancel
+                  className="delete-button"
+                  onClick={() => removeItem(item.id)}
+                />
+              </li>
+            );
+          })}
+        </ul>
+        <button onClick={handleInputDisplay} className="plus">
+          <AiOutlinePlus />
+        </button>
+      </div>
     </div>
-    
-
-
-{/* List of Items */}
-
-<ul >
-  {items.map(item => {
-    return(
-      <li key={item.id} >
-        {item.value}
-        <img src={require("./cancel.png")}
-         height="18" width="18" alt="fgefb"
-         className="delete-button" onClick={() => removeItem(item.id)}
-         />
-       
-      </li>
-    )
-  })}
-
-</ul>
-<img src= {require("./plus.png")}height="40" width="40" alt="plus"  className="plus" />
-
-</div>
-  </div>
-)
-
+  );
 }
-
-
-
-
 
 export default App;
